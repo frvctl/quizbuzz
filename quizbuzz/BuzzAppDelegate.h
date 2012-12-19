223efe58af76c4d095979f6b50f873fc8218d64f
epiccodez <epiccodez@gmail.com>
2012-12-19 16:55:06 -0500

    Application handler, basic

--- quizbuzz/BuzzAppDelegate.h
+++ quizbuzz/BuzzAppDelegate.h
@@ -7,9 +7,38 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "hidapi.h"
 
-@interface BuzzAppDelegate : NSObject <NSApplicationDelegate>
+@interface BuzzAppDelegate : NSObject <NSApplicationDelegate> {
+
+    int res;
+    int lastBuzzed;
+    int buzzNum;
+    
+    NSString *text;
+    
+    bool listen;
+
+    unsigned char buffer[65];
+    unsigned char buf_temp[65];
+    
+    hid_device  *handle;
+    
+    __unsafe_unretained NSTextView *mainTextArea;
+}
+
 
 @property (assign) IBOutlet NSWindow *window;
+@property (unsafe_unretained) IBOutlet NSTextView *mainTextArea;
+
+
+- (IBAction)resetBuzzerButton:(id)sender;
+- (IBAction)stopReading:(id)sender;
+- (IBAction)listenForEventsEngage:(id)sender;
+ 
+- (void)listenToTheBuzzer;
+- (void)lightTheBuzz:(int)buzzerToLight event:(bool)isAEvent;
+- (void)resetTheBuzzer;
+- (void)createBuzzerConnection;
 
 @end
